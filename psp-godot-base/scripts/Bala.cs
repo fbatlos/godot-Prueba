1be404a15b89a3d512e50c4ea15a7c3ec6537a0e
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-11-05 22:50:45 +0100

    Primer comit

---
+++ psp-godot-base/scripts/Bala.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Bala : Area2D
+{
+	[Export]
+	public float Speed = 1.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Position += new Vector2(Speed + (float)delta,0.0f);
+	}
+	
+	
+}
---
+++ psp-godot-base/scripts/CharacterBody2d.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+public partial class CharacterBody2d : CharacterBody2D
+{
+	[Export]
+	public float Speed = 300.0f;
+	[Export]
+	public float JumpVelocity = -400.0f;
+	
+	private AnimatedSprite2D animation;
+	
+	private PackedScene bala;
+	
+	public override void _Ready(){
+		animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		bala = GD.Load<PackedScene>("res://scenes/bala.tscn");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+			animation.Play("jump");
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_up") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+			
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			animation.FlipH = velocity.X < 0;
+			if (IsOnFloor()){
+				animation.Play("walk");
+			}
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			if (IsOnFloor()){
+				animation.Play("idle");
+			}
+		}
+		
+		if(Input.IsActionJustPressed("fire")){
+			GD.Print("Disparo");
+			Node2D instBullet = (Node2D)bala.Instantiate();
+			instBullet.GlobalPosition = GlobalPosition;
+			
+
+			GetTree().Root.AddChild(instBullet);
+
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
