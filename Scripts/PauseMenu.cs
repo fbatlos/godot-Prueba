09883cd01b8dbe604896728a93dd74d0403d49b1
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-12-12 10:44:12 +0100

    Se añadió la pausa, idea de meter algun power up

---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public partial class PauseMenu : Control
+{
+	[Export]
+	AnimationPlayer animation;
+	
+	public void Resume()
+	{
+		GetTree().Paused = false;
+		animation.PlayBackwards("blur");
+	}
+
+	public void Pause()
+	{
+		GetTree().Paused = true;
+		animation.Play("blur");
+	}
+	
+	public void TestEsc()
+	{
+		if (Input.IsActionJustPressed("esc") && !GetTree().Paused)
+		{
+			Pause();
+		}
+		else if (Input.IsActionJustPressed("esc") && GetTree().Paused)
+		{
+			Resume();
+		}
+	}
+	
+	
+	public void _on_Resume(){
+		Resume();
+	}
+	
+	public void _on_Restart(){
+		Resume();
+		GetTree().ReloadCurrentScene();
+	}
+	
+	public void _on_Quit(){
+		GetTree().Quit();
+	}
+	
+	public override void _Process(double delta)
+	{
+		TestEsc();
+	}
+	
+	public override void _Ready()
+	{
+		animation.Play("RESET");
+	}
+
+
+}
